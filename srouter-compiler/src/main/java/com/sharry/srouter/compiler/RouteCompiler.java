9d00534825dfe822e7d7b91f27d750452e5a08e1
sharrychoo <sharrychoo@outlook.com>
2019-05-24 19:45:30 +0800

    docs($compiler): 更新 Router androidx 的 fragment 的文本描述

--- srouter-compiler/src/main/java/com/sharry/srouter/compiler/RouteCompiler.java
+++ srouter-compiler/src/main/java/com/sharry/srouter/compiler/RouteCompiler.java
@@ -205,7 +205,7 @@ public class RouteCompiler extends BaseCompiler {
             writeLoadInto(loadInto, "FRAGMENT_V4", authority, path, interceptorURIs, element);
         } else if (types.isSubtype(tm, typeFragmentX)) {
             // @Route bind class is child for Fragment.
-            logger.i("Found fragment v4 route: " + tm.toString() + " <<<");
+            logger.i("Found fragment x route: " + tm.toString() + " <<<");
             writeLoadInto(loadInto, "FRAGMENT_X", authority, path, interceptorURIs, element);
         } else if (types.isSubtype(tm, typeService)) {
             // @Route bind class is child for IService.
